265f8ea74a918956725567dd46334c8582c4f657
tectiv3 <git@getpanda.me>
2020-01-26 20:28:23 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,11 @@
+module tectiv3/dht
+
+go 1.12
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
+	github.com/kidoman/embd v0.0.0-20170508013040-d3d8c0c5c68d // indirect
+	github.com/morus12/dht22 v0.0.0-20170211005327-87805f7fca72
+	golang.org/x/net v0.0.0-20200114155413-6afb5195e5aa // indirect
+)
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/morus12/dht22"
+)
+
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	log.Printf("TOPIC: %s\n", msg.Topic())
+	log.Printf("MSG: %s\n", msg.Payload())
+}
+
+func main() {
+	// mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	// mqtt.ERROR = log.New(os.Stdout, "", 0)
+	opts := mqtt.NewClientOptions().AddBroker("tcp://192.168.100.111:1883").SetClientID("pidht")
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+
+	c := mqtt.NewClient(opts)
+	if token := c.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
+	log.Println("Connected to mqtt")
+
+	sensor := dht22.New("GPIO_26")
+
+	c1 := make(chan struct{}, 1)
+	go func() {
+		temperature, err := sensor.Temperature()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(temperature - 1)
+		humidity, err := sensor.Humidity()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Println(humidity)
+		if temperature < 0 || humidity < 0 {
+			os.Exit(0)
+		}
+		token := c.Publish("sensors/node_bedroom/last", 0, true, fmt.Sprintf(`{"temperature": %0.2f,"humidity": %0.2f}`, temperature-1, humidity))
+		token.Wait()
+		c1 <- struct{}{}
+	}()
+
+	select {
+	case <-c1:
+		log.Println("done")
+	case <-time.After(15 * time.Second):
+		log.Println("timeout...")
+		os.Exit(1)
+	}
+
+}
